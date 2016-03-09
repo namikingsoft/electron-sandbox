7798afb5a146afd9eb21051dfdba2b739583ffbc
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-09 23:52:24 +0900

    Try Object.freeze on domain entity

--- src/domains/Letter.ts
+++ src/domains/Letter.ts
@@ -1,7 +1,10 @@
 export default class Letter {
   constructor(private param: {
     text: string,
-  }) {}
+  }) {
+    Object.freeze(this)
+    Object.freeze(this.param)
+  }
 
   get text(): string {
     return this.param.text
--- src/domains/Post.ts
+++ src/domains/Post.ts
@@ -4,7 +4,10 @@ import Letter from './Letter'
 export default class Post {
   constructor(private param: {
     letters: List<Letter>
-  }) {}
+  }) {
+    Object.freeze(this)
+    Object.freeze(this.param)
+  }
 
   get letters(): List<Letter> {
     return this.param.letters
