4c06e976754b642772650f5896f6c1d8c7ac88c3
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-12 21:26:24 +0900

    refactor(gulp): integrate script.js from test.js

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -8,7 +8,7 @@ const project = $.typescript.createProject('tsconfig.json', {
 
 const build = (src, dest, isWatch) => {
   const isDevelop = dest !== config.dir.dist
-  return gulp.src(config.src.script)
+  return gulp.src(src)
   .pipe($.if(isWatch, $.plumber()))
   .pipe($.if(isDevelop, $.sourcemaps.init()))
   .pipe($.typescript(project))
@@ -26,5 +26,15 @@ gulp.task('script:server:watch',  () => {
   $.watch(config.src.script, config.watch, file => {
     // @todo incremental build of typescript is difficult
     build(config.src.script, config.dir.work.server, true)
+  })
+})
+
+gulp.task('script:test', () => {
+  build([config.src.script, config.src.test], config.dir.work.test)
+})
+gulp.task('script:test:watch', () => {
+  $.watch([config.src.script, config.src.test], config.watch, () => {
+    // @todo incremental build of typescript is difficult
+    build([config.src.script, config.src.test], config.dir.work.test, true)
   })
 })
