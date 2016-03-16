53386144177ef9181b71bf4dd911a76c4fa518a7
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-16 16:24:11 +0900

    test(domain): add test of some domain class

--- test/domains/Letter.spec.ts
+++ test/domains/Letter.spec.ts
@@ -1,4 +1,7 @@
 import Letter from 'domains/Letter'
+import User from 'domains/User'
+import Channel from 'domains/Channel'
+import Message from 'domains/Message'
 import * as assert from 'power-assert'
 
 describe('Letter', function() {
@@ -6,28 +9,29 @@ describe('Letter', function() {
   let letter: Letter
 
   before(() => {
+    const user = new User({
+      id: 'ID',
+      name: 'User',
+    })
+    const channel = new Channel({
+      id: 'ID',
+      name: 'Channel',
+    })
+    const message = new Message('Text')
     letter = new Letter({
-      text: 'Text',
-      user: 'User',
-      channel: 'Channel',
+      user, channel, message,
     })
   })
 
   describe('new', () => {
-    it('should be new instance', () => {
+    it('should be return new instance', () => {
       assert(letter instanceof Letter)
     })
   })
 
   describe('id', () => {
     it('should be return identify string', () => {
-      assert(/[0-9a-f\-]+/.test(letter.id.toString()))
-    })
-  })
-
-  describe('text', () => {
-    it('should be return initial value', () => {
-      assert(letter.text === 'Text')
+      assert(/[0-9a-f\-]+/.test(letter.id))
     })
   })
 
@@ -40,6 +44,12 @@ describe('Letter', function() {
   describe('channel', () => {
     it('should be return initial value', () => {
       assert(letter.channel === 'Channel')
+    })
+  })
+
+  describe('message', () => {
+    it('should be return initial value', () => {
+      assert(letter.message === 'Text')
     })
   })
 
