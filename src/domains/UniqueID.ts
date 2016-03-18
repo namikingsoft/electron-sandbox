3d161947ca91a8d56b70cff242881046e2a473f4
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-18 20:34:59 +0900

    refactor(domain): around UniqueID

---
+++ src/domains/UniqueID.ts
@@ -0,0 +1,10 @@
+import * as uuid from 'node-uuid'
+import SingleValue from '../domains/SingleValue'
+import freeze from '../decorators/freeze'
+
+@freeze
+export default class UniqueID extends SingleValue<string> {
+  constructor(uid: string = null) {
+    super(uid || uuid.v1())
+  }
+}
