a2d6316eb198ca6e3b136e17aabbcc7036895146
Tsubasa Namiki <namikingsoft@gmail.com>
2016-03-21 01:18:43 +0900

    feat(component): add typing animation

--- src/app.const.ts
+++ src/app.const.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = `file://${__dirname}/index.html`
 export const STREAM_TRANS_MSEC = 10000
 export const TYPING_TRANS_MSEC = 10000
+export const TYPING_DELTA_MSEC = 15
 export const NO_IMAGE_URL = 'http://dummyimage.com/100x100/eee/333&text=NoImage'
